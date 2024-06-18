6b18045c77b9f55b937f7cbf89d5122e544dcc46
odilov2 <odilovelshodbek0703@gmail.com>
2024-06-18 21:40:31 +0500

    refreshed

--- FAST_API/api/product.py
+++ FAST_API/api/product.py
@@ -136,3 +136,4 @@ async def delete_product(id: int):
         session.commit()
         return HTTPException(status_code=status.HTTP_204_NO_CONTENT, detail="Product deleted successfully")
     return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product not found")
+
